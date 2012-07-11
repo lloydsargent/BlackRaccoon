1adda7a015b6d7bda64ffdbf2283821e031641fc
Lloyd Sargent <lloyd.sargent@cannasoftware.com>
2012-07-11 11:44:17 -0500

    Initial Commit

---
+++ BlackRaccoon/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  BlackRaccoon
+//
+//  Created by Lloyd Sargent on 7/11/12.
+//  Copyright (c) 2012 Canna Software Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlackRaccoon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlackRaccoon
+//
+//  Created by Lloyd Sargent on 7/11/12.
+//  Copyright (c) 2012 Canna Software Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
